dd1df98a743d4003c725ef30d8ef745a83082bcb
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-22 09:57:04 -0300

    Projeto: Lista de contatos - Excluindo contato

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -42,4 +42,29 @@ router.get("/contatos", async (req, res) => {
     res.json({ contatos: list });
 });
 
+
+router.delete("/contato", async (req, res) => {
+    const { name } = req.query;
+
+    if (!name) {
+        res.json({ error: "Precisa mandar um nome para excluir." });
+        return;
+    }
+
+    let list: string[] = [];
+
+    try {
+        const data = await readFile(dataSource, { encoding: "utf-8" });
+        list = data.split("\n");
+    } catch (error) {
+
+    }
+
+    list = list.filter(item => item.toLowerCase() !== (name as string).toLowerCase());
+
+    await writeFile(dataSource, list.join("\n"));
+    res.json({ contato: name });
+
+});
+
 export default router;
