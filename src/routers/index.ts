406e8c2cd07d772da681c733ef2c3cae1dac4d3c
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-22 09:40:15 -0300

    Projeto: Lista de contatos - Inserindo contato

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,5 +1,31 @@
 import express from "express";
+import { readFile, writeFile } from "fs/promises";
 
+const dataSource = './data/list.txt';
 const router = express.Router();
+
+router.post("/contato", async (req, res) => {
+    const { name } = req.body;
+
+    if (!name || name.length < 2) {
+        res.json({ error: "Nome precisa ter pelo menos 2 caracteres." });
+        return;
+    }
+
+    let list: string[] = [];
+
+    try {
+        const data = await readFile(dataSource, {encoding:"utf-8"});
+        list = data.split("\n");
+    } catch (error) {
+        
+    }
+
+    list.push(name);
+
+    await writeFile(dataSource, list.join("\n"));
+
+    res.status(201).json({ contato: name });
+});
 
 export default router;
